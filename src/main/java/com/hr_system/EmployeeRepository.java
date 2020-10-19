c5b9999eb1145b33f2126cd6ea7cac9d76e33965
talita <polikashina@gmail.com>
2020-10-19 10:12:17 +0200

    add crud repository class

---
+++ src/main/java/com/hr_system/EmployeeRepository.java
@@ -0,0 +1,9 @@
+package com.hr_system;
+
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface EmployeeRepository extends CrudRepository<Employee, Long> {
+
+}
