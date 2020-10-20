fea9608cb31a862100184f26988b50b5faeb66bb
talita <polikashina@gmail.com>
2020-10-20 16:21:36 +0200

    add autowired repository test

--- src/test/java/com/hr_system/HrSystemApplicationTests.java
+++ src/test/java/com/hr_system/HrSystemApplicationTests.java
@@ -1,13 +1,35 @@
 package com.hr_system;
 
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.springframework.test.util.AssertionErrors.assertEquals;
+
 import org.junit.jupiter.api.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
-class HrSystemApplicationTests {
+import org.springframework.test.context.junit4.SpringRunner;
+
+import java.util.Optional;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest(classes = HrSystemApplication.class)
+public class HrSystemApplicationTests {
+
+	@Autowired
+	private EmployeeController controller;
+
+	@Autowired
+	private EmployeeRepository repository;
 
 	@Test
-	void contextLoads() {
-	}
+	public void testGivenGenericEntityRepository() {
+		Employee genericEntity = repository
+				.save(new Employee("test", 34, "Test position"));
+		Optional<Employee> foundEntity = repository
+				.findById(genericEntity.getId());
 
+		assertNotNull(foundEntity);
+		assertEquals("employees muse be equal", genericEntity.getName(), "test");
+	}
 }
